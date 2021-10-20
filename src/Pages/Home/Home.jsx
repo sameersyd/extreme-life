01dee769426bce9ee2103ab1415b539ab493a168
Sameer Nawaz <sameer.nwaz@gmail.com>
2021-10-20 12:57:13 -0700

    Update UI: Home bottom container

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -11,10 +11,12 @@ class Home extends Component {
                     <h1 className="home-title">the</h1>
                     <h1 className="home-title">Board</h1>
                 </div>
-                <div className="home-bottom-container">
-                    <h1 className="home-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ultricies ultrices tincidunt nam augue non blandit cursus proin non. Non volutpat vulputate tincidunt nunc </h1>
-                    <h1 className="home-primaryBtn">Let's Go</h1>
-                    <h1 className="home-secondaryBtn">About Us</h1>
+                <div className="home-bottom-wrap">
+                    <div className="home-bottom-container">
+                        <h1 className="home-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ultricies ultrices tincidunt nam augue non blandit cursus proin non. Non volutpat vulputate tincidunt nunc </h1>
+                        <h1 className="home-primaryBtn">Let's Go</h1>
+                        <h1 className="home-secondaryBtn">How to play?</h1>
+                    </div>
                 </div>
             </div>
         )
