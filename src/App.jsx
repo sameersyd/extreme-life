61e130c2172de72cc9a7dd1d2f3ddf0ffecd7146
Sameer Nawaz <sameer.nwaz@gmail.com>
2021-10-20 11:43:52 -0700

    Setup Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Home from './Pages/Home/Home.jsx';
+
 class App extends Component {
 	render() {
         return(
-            <div>
-                <h1 className="hello-world">Hello World</h1>
-            </div>
+            <Home/>
         )
     }
 }
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Home extends Component {
+	render() {
+        return(
+            <div>
+                <h1 className="hello-world">Hello World</h1>
+            </div>
+        )
+    }
+}
+
+export default Home;
