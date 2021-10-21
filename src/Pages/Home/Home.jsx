5db7fb3498c043dc1d7476e6a8102f8efd9ab3b1
Sameer Nawaz <sameer.nwaz@gmail.com>
2021-10-21 11:26:57 -0700

    Add box design in landing page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,20 +4,23 @@ import './Home.css';
 class Home extends Component {
 	render() {
         return(
-            <div className="home-container">
-                <div className="home-logoImage"/>
-                <div className="home-title-container">
-                    <h1 className="home-title">Conquer</h1>
-                    <h1 className="home-title">the</h1>
-                    <h1 className="home-title">Board</h1>
-                </div>
-                <div className="home-bottom-wrap">
-                    <div className="home-bottom-container">
-                        <h1 className="home-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ultricies ultrices tincidunt nam augue non blandit cursus proin non. Non volutpat vulputate tincidunt nunc </h1>
-                        <h1 className="home-primaryBtn">Let's Go</h1>
-                        <h1 className="home-secondaryBtn">How to play?</h1>
+            <div>
+                <div className="home-container">
+                    <div className="home-logoImage"/>
+                    <div className="home-title-container">
+                        <h1 className="home-title">Conquer</h1>
+                        <h1 className="home-title">the</h1>
+                        <h1 className="home-title">Board</h1>
+                    </div>
+                    <div className="home-bottom-wrap">
+                        <div className="home-bottom-container">
+                            <h1 className="home-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ultricies ultrices tincidunt nam augue non blandit cursus proin non. Non volutpat vulputate tincidunt nunc </h1>
+                            <h1 className="home-primaryBtn">Let's Go</h1>
+                            <h1 className="home-secondaryBtn">How to play?</h1>
+                        </div>
                     </div>
                 </div>
+                <div className="home-boxes"/>
             </div>
         )
     }
