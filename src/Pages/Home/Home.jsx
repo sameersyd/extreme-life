448ffcb388d73fac9c2a7b8cb78d1ebe95f45e22
Sameer Nawaz <sameer.nwaz@gmail.com>
2021-10-25 21:21:49 -0700

    Squashed commit of the following:
    commit 011d915e57397dc6d0563d2a676f9fc5231b7c4e
    Author: Sameer Nawaz <sameer.nwaz@gmail.com>
    Date:   Mon Oct 25 21:19:40 2021 -0700
    
        Add startDialog generate button
    
    commit ee4fdce2058df1d18b602d4c43e0b46a6adee852
    Author: Sameer Nawaz <sameer.nwaz@gmail.com>
    Date:   Mon Oct 25 20:54:41 2021 -0700
    
        Add selection script and action buttons
    
    commit d4dcb428b2ab542b3f9d4b649749c2cf0803dc16
    Author: Sameer Nawaz <sameer.nwaz@gmail.com>
    Date:   Mon Oct 25 20:29:37 2021 -0700
    
        Add startDialog selection title
    
    commit 8fadcd3729986ff33d958e7ff15ff7908126171e
    Author: Sameer Nawaz <sameer.nwaz@gmail.com>
    Date:   Mon Oct 25 20:11:11 2021 -0700
    
        Add react spring

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Home.css';
+import { Spring, animated, interpolate } from 'react-spring'
 
 class Home extends Component {
 
@@ -7,7 +8,7 @@ class Home extends Component {
         super(props)
         this.state = {
             isLoading: false,
-            showStartDialog: true,
+            isStartDialogOpen: false,
             nickname: ""
         }
     }
@@ -15,7 +16,7 @@ class Home extends Component {
 	render() {
         return(
             <div className={this.state.isLoading ? "home-disableInteraction" : ""}>
-                <div className={this.state.showStartDialog ? "home-container home-bgBlur" : "home-container"}>
+                <div className={this.state.isStartDialogOpen ? "home-container home-bgBlur" : "home-container"}>
                     <div className="home-logoImage"/>
                     <div className="home-title-container">
                         <h1 className="home-title">Conquer</h1>
@@ -25,22 +26,43 @@ class Home extends Component {
                     <div className="home-bottom-wrap">
                         <div className="home-bottom-container">
                             <h1 className="home-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ultricies ultrices tincidunt nam augue non blandit cursus proin non. Non volutpat vulputate tincidunt nunc </h1>
-                            <h1 className="home-primaryBtn" onClick={(e) => { this.setState({showStartDialog: true}) }}>Let's Go</h1>
+                            <h1 className="home-primaryBtn" onClick={(e) => { this.setState({isStartDialogOpen: true}) }}>Let's Go</h1>
                             <h1 className="home-secondaryBtn">How to play?</h1>
                         </div>
                     </div>
                 </div>
-                <div className={this.state.showStartDialog ? "home-boxes home-bgBlur" : "home-boxes"}/>
-                <div className="home-startDialog">
-                    <div>
-                        <input type="text" className="home-startDialog-input" placeholder="Nick name" onChange={event => this.setState({nickname: event.target.value})} />
-                        <div class="home-startDialog-divider"/>
-                    </div>
-                    <div>
-                        <h1 className="home-text">Select actions</h1>
-                        <h1 className="home-text">Submit script</h1>
-                    </div>
-                </div>
+                <div className={this.state.isStartDialogOpen ? "home-boxes home-bgBlur" : "home-boxes"}/>
+                <Spring
+                    native
+                    from= {{ transform: 'translate(-50%, 200%)' }}
+                    to={{ transform: this.state.isStartDialogOpen ? 'translate(-50%, -50%)' : 'translate(-50%, 200%)' }}
+                >
+                {
+                    (animation)=>(
+                        <animated.div style={animation} className="home-startDialog">
+                            <div>
+                                <div className="home-startDialog-input-dialog">
+                                    <input type="text" className="home-startDialog-input" placeholder="Nick name" onChange={event => this.setState({nickname: event.target.value})} />
+                                    <div className="home-startDialog-generate"/>
+                                </div>
+                                <div class="home-startDialog-divider"/>
+                            </div>
+                            <div className="home-startDialog-selection-body">
+                                <div className="home-startDialog-selection">
+                                    <div className="home-startDialog-selection-actions"/>
+                                    <h1 className="home-startDialog-selection-title">Select actions</h1>
+                                </div>
+                                <div className="home-startDialog-selection">
+                                    <div className="home-startDialog-selection-script"/>
+                                    <h1 className="home-startDialog-selection-title">Submit script</h1>
+                                </div>
+                            </div>
+                            <h1 className="home-startDialog-startBtn">Start Game</h1>
+                            <h1 className="home-startDialog-cancel" onClick={(e) => { this.setState({isStartDialogOpen: false}) }}>Cancel</h1>
+                        </animated.div>
+                    )
+                }
+                </Spring>
             </div>
         )
     }
